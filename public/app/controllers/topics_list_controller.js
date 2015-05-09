591413ff6233d1165404709cdceeb422c23187a7
pppak <pia.pakarinen@helsinki.fi>
2015-05-09 13:22:24 +0300

    t51

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,12 @@
-FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('TopicsListController', function ($scope, $location, Api) {
+    // Toteuta kontrolleri tähän
+    Api.getTopics().success(function (topics) {
+        $scope.topics = topics;
+    });
+
+    $scope.addTopic = function (){      
+        Api.addTopic({name: $scope.newTopic.name, description: $scope.newTopic.description}).success(function(topic){
+            $location.path('/topics/' + topic.$id);
+        });
+    }
 });
